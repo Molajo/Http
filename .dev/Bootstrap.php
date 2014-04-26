ab2f82148ffbfb52601ac0af58e8cf146ec19e9f
AmyStephen <AmyStephen@gmail.com>
2014-04-26 10:23:15 -0500

    Packaging

--- .dev/Bootstrap.php
+++ .dev/Bootstrap.php
@@ -6,29 +6,14 @@
  * @copyright  2014 Amy Stephen. All rights reserved.
  * @license    http://www.opensource.org/licenses/mit-license.html MIT License
  */
-include_once __DIR__ . '/CreateClassMap.php';
-
-if (! defined('PHP_VERSION_ID')) {
-    $version = explode('.', phpversion());
-    define('PHP_VERSION_ID', ($version[0] * 10000 + $version[1] * 100 + $version[2]));
-}
-
 $base     = substr(__DIR__, 0, strlen(__DIR__) - 5);
-$classmap = array();
-$classmap = createClassMap($base . '/vendor/commonapi/http', 'CommonApi\\Http\\');
-$results  = createClassMap(
-    $base . '/vendor/commonapi/exception',
-    'CommonApi\\Exception\\'
-);
-$classmap = array_merge($classmap, $results);
-
+include_once $base . '/vendor/autoload.php';
+include_once __DIR__ . '/CreateClassMap.php';
 $classmap['Molajo\\Http\\Client']   = $base . '/Source/Client.php';
-$classmap['Molajo\\Http\\Redirect'] = $base . '/Source/Redirect.php';
 $classmap['Molajo\\Http\\Request']  = $base . '/Source/Request.php';
 $classmap['Molajo\\Http\\Response'] = $base . '/Source/Response.php';
 $classmap['Molajo\\Http\\Server']   = $base . '/Source/Server.php';
 $classmap['Molajo\\Http\\Upload']   = $base . '/Source/Upload.php';
-ksort($classmap);
 
 spl_autoload_register(
     function ($class) use ($classmap) {
