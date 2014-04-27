58b690e2ae62df38b7b3a63124b93f4ab465f30d
AmyStephen <AmyStephen@gmail.com>
2014-04-27 05:40:01 -0500

    Data Gathering Process

--- .dev/Bootstrap.php
+++ .dev/Bootstrap.php
@@ -6,9 +6,13 @@
  * @copyright  2014 Amy Stephen. All rights reserved.
  * @license    http://www.opensource.org/licenses/mit-license.html MIT License
  */
-$base     = substr(__DIR__, 0, strlen(__DIR__) - 5);
+$base = substr(__DIR__, 0, strlen(__DIR__) - 5);
+if (function_exists('CreateClassMap')) {
+} else {
+    include_once __DIR__ . '/CreateClassMap.php';
+}
 include_once $base . '/vendor/autoload.php';
-include_once __DIR__ . '/CreateClassMap.php';
+
 $classmap['Molajo\\Http\\Client']   = $base . '/Source/Client.php';
 $classmap['Molajo\\Http\\Request']  = $base . '/Source/Request.php';
 $classmap['Molajo\\Http\\Response'] = $base . '/Source/Response.php';
